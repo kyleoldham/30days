045fd9547e821267d65116a4753b90b1a933798e
kyle <kyleoldham.4@gmail.com>
2018-06-25 16:14:46 -0700

    HR - day 8

--- 30days/day8.go
+++ 30days/day8.go
@@ -3,19 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var array_length int
-	fmt.Scanf("%d", &array_length)
+	// Create map and specify number of inputs
+	phoneBook := make(map[string]int)
+	var num int
+	fmt.Scanf("%d", &num)
 
-	// Create array
-	var array = make([]int, array_length)
-
-	// Take in array input
-	for i := 0; i < array_length; i++ {
-		fmt.Scanf("%d", &array[i])
+	for i := 0; i < num; i++ {
+		// Set the map up with inputs
+		var name string
+		var phoneNumber int
+		fmt.Scanf("%s", &name)
+		fmt.Scanf("%d", &phoneNumber)
+		phoneBook[name] = phoneNumber
 	}
 
-	// Swippity swap
-	for i := 1; i <= len(array); i++ {
-		fmt.Printf("%d ", array[array_length-i])
+	// Look up name in map and print if found
+	for {
+		var lookup_contact string
+		// Was getting timeouts without googling how to do this..
+		_, err := fmt.Scanf("%s", &lookup_contact)
+		if err != nil {
+			break
+		}
+		phoneNumber, exist := phoneBook[lookup_contact]
+		if exist {
+			fmt.Printf("%s=%d\n", lookup_contact, phoneNumber)
+		} else {
+			fmt.Println("Not found")
+		}
 	}
 }
